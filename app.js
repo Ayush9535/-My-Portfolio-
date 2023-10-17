f6d380a5298e7813876fe430a282e332101f3b3a
Ayush9535 <ghodkeayush22@gmail.com>
2023-10-18 03:48:55 +0530

    added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let array = [
     "hangman.png",
     "calc.png",
     "temp.png",
+    "car rental.png",
     "sdms.png"
 ]
 
@@ -9,6 +10,7 @@ let array1 = [
     "Hangman Game",
     "Calculator",
     "Temperature Convertor",
+    "Car Rental Website",
     "Student Data Management System"
 ]
 
@@ -16,12 +18,13 @@ let array5 = [
     "Hangman is a classic word-guessing game where player has to guess a word choosed by computer. For each incorrect guess, a part of a stick figure is drawn (representing a hanging man). The guessing player's goal is to guess the word before the entire stick figure is drawn, usually within a limited number of guesses..",
     "This is a simple calculator project using HTML, CSS, and JavaScript involves performing basic calculations or arithmetic operations like addition, subtraction, multiplication, and division etc",
     "A temperature converter project using HTML, CSS, and JavaScript is an application that allows users to convert temperatures between different units such as Celsius, Fahrenheit, and Kelvin.Users can input a temperature in one unit and select the desired unit to convert to, and the application will calculate and display the converted temperature.",
+    "Welcome to our Car Rental Website, a sleek and modern project crafted with HTML and CSS. Explore our intuitive interface as you seamlessly navigate through our curated car rental offerings. With clean, structured code and a visually appealing design, booking your desired ride has never been easier. ",
     "This is project made using Python Tkinter for GUI and MySql for handling CRUD operations on tables. In this you can add , update , delete , display student data. Here Data is stored in form of tables in databases."
 ]
 
-let array2 = ["https://github.com/Ayush9535/Hangman-Game" , "https://github.com/Ayush9535/Calculator" , "https://github.com/Ayush9535/Temperature-Convertor" , "https://github.com/Ayush9535/Student-Data-Management-System"]
+let array2 = ["https://github.com/Ayush9535/Hangman-Game" , "https://github.com/Ayush9535/Calculator" , "https://github.com/Ayush9535/Temperature-Convertor" , "https://github.com/Ayush9535/Car-Rental-Website" , "https://github.com/Ayush9535/Student-Data-Management-System"]
 
-let array3 = ["https://hangman-game-project1.netlify.app/" , "https://calc-website.netlify.app/", "https://temp-convert-website.netlify.app/" , "https://www.google.com"]
+let array3 = ["https://hangman-game-project1.netlify.app/" , "https://calc-website.netlify.app/", "https://temp-convert-website.netlify.app/" , "https://car-rental-website-project.netlify.app/" , "https://www.google.com"]
 
 const contactform = document.getElementById("cont")
 const resulttext = document.getElementById("result-text")
@@ -93,19 +96,19 @@ function next(){
     let b = array1.indexOf(projectname.innerHTML) + 1
     let e = array5.indexOf(info.innerHTML) + 1
 
-    if (a === 4){
+    if (a === 5){
         a = 0
     }
-    if(b === 4){
+    if(b === 5){
         b = 0
     }
-    if(c === 4){
+    if(c === 5){
         c = 0
     }
-    if(d === 4){
+    if(d === 5){
         d = 0
     }
-    if(e===4){
+    if(e===5){
         e=0
     }
     projectimg.setAttribute("src" , array[a])
@@ -123,19 +126,19 @@ function previous(){
     let e = array5.indexOf(info.innerHTML) - 1
 
     if (a == -1){
-        a = 3
+        a = 4
     }
     if(b == -1){
-        b = 3
+        b = 4
     }
     if(c == -1){
-        c = 3
+        c = 4
     }
     if(d == -1){
-        d = 3
+        d = 4
     }
     if(e == -1){
-        e = 3
+        e = 4
     }
     projectimg.setAttribute("src" , array[a])
     demolink.setAttribute("href" , array3[c])
