14e6cf373ff2cc6930b69b056b60cbf6727d0ec3
Ayush9535 <ghodkeayush22@gmail.com>
2023-12-25 19:45:30 +0530

    added

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ let array1 = [
     "Student Data Management System"
 ]
 
-let array5 = [
-    "Hangman is a classic word-guessing game where player has to guess a word choosed by computer. For each incorrect guess, a part of a stick figure is drawn (representing a hanging man). The guessing player's goal is to guess the word before the entire stick figure is drawn, usually within a limited number of guesses..",
-    "This is a simple calculator project using HTML, CSS, and JavaScript involves performing basic calculations or arithmetic operations like addition, subtraction, multiplication, and division etc",
-    "A temperature converter project using HTML, CSS, and JavaScript is an application that allows users to convert temperatures between different units such as Celsius, Fahrenheit, and Kelvin.Users can input a temperature in one unit and select the desired unit to convert to, and the application will calculate and display the converted temperature.",
-    "Welcome to our Car Rental Website, a sleek and modern project crafted with HTML and CSS. Explore our intuitive interface as you seamlessly navigate through our curated car rental offerings. With clean, structured code and a visually appealing design, booking your desired ride has never been easier. ",
-    "This is project made using Python Tkinter for GUI and MySql for handling CRUD operations on tables. In this you can add , update , delete , display student data. Here Data is stored in form of tables in databases."
-]
-
 let array2 = ["https://github.com/Ayush9535/Hangman-Game" , "https://github.com/Ayush9535/Calculator" , "https://github.com/Ayush9535/Temperature-Convertor" , "https://github.com/Ayush9535/Car-Rental-Website" , "https://github.com/Ayush9535/Student-Data-Management-System"]
 
 let array3 = ["https://hangman-game-project1.netlify.app/" , "https://calc-website.netlify.app/", "https://temp-convert-website.netlify.app/" , "https://car-rental-website-project.netlify.app/" , "https://www.google.com"]
@@ -87,64 +79,6 @@ function contactsubmit(){
         submitbutton.classList.add("hide")
         resulttext.innerHTML = "Thank You..!! <br>  I will contact you soon."
     }
-}
-
-function next(){
-    let a = array.indexOf(projectimg.getAttribute("src")) + 1
-    let c = array3.indexOf(demolink.getAttribute("href")) + 1
-    let d = array2.indexOf(codelink.getAttribute("href")) + 1
-    let b = array1.indexOf(projectname.innerHTML) + 1
-    let e = array5.indexOf(info.innerHTML) + 1
-
-    if (a === 5){
-        a = 0
-    }
-    if(b === 5){
-        b = 0
-    }
-    if(c === 5){
-        c = 0
-    }
-    if(d === 5){
-        d = 0
-    }
-    if(e===5){
-        e=0
-    }
-    projectimg.setAttribute("src" , array[a])
-    demolink.setAttribute("href" , array3[c])
-    codelink.setAttribute("href" , array2[d])
-    projectname.innerHTML = array1[b]
-    info.innerHTML = array5[e]
-}
-
-function previous(){
-    let a = array.indexOf(projectimg.getAttribute("src")) - 1
-    let c = array3.indexOf(demolink.getAttribute("href")) - 1
-    let d = array2.indexOf(codelink.getAttribute("href")) - 1
-    let b = array1.indexOf(projectname.innerHTML) - 1
-    let e = array5.indexOf(info.innerHTML) - 1
-
-    if (a == -1){
-        a = 4
-    }
-    if(b == -1){
-        b = 4
-    }
-    if(c == -1){
-        c = 4
-    }
-    if(d == -1){
-        d = 4
-    }
-    if(e == -1){
-        e = 4
-    }
-    projectimg.setAttribute("src" , array[a])
-    demolink.setAttribute("href" , array3[c])
-    codelink.setAttribute("href" , array2[d])
-    projectname.innerHTML = array1[b]
-    info.innerHTML = array5[e]
 }
 
 const goup1 = document.getElementById("goup1")
